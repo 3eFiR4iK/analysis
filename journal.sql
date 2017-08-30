1b559f9b3dfa937e310388203c06b10cf56e2ca7
3eFiR4iK <abushinov97@gmail.com>
2017-08-30 16:47:30 +0300

    modified:   journal.sql

--- journal.sql
+++ journal.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 28 2017 г., 23:55
+-- Время создания: Авг 30 2017 г., 16:25
 -- Версия сервера: 5.5.50
 -- Версия PHP: 5.6.23
 
@@ -50,17 +50,35 @@ CREATE TABLE IF NOT EXISTS `event` (
   `name` text NOT NULL,
   `count` int(11) NOT NULL DEFAULT '0',
   `category_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `event`
 --
 
 INSERT INTO `event` (`id`, `name`, `count`, `category_id`) VALUES
-(1, 'обучение и консультации преподавателей и сотрудников работе в системе управления обучением «LMS-Школа»', 1, 1),
-(2, 'обучение сотрудников СПбКВК работе с электронным документооборота OnlyOffice', 12, 1),
-(3, 'консультации для преподавателей и воспитателей по работе с электронным документооборота OnlyOffice', 27, 1),
-(4, 'оказание оперативной помощи сотрудникам СПбКВК (не включается, «тормозит», не работает)', 148, 2);
+(5, 'ремонт пк', 1, 2),
+(6, 'Выпрямление рук', 7, 2),
+(7, 'Перенос техники', 1, 2),
+(8, 'Выходной', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `room`
+--
+
+CREATE TABLE IF NOT EXISTS `room` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `room`
+--
+
+INSERT INTO `room` (`id`, `name`) VALUES
+(1, '101');
 
 -- --------------------------------------------------------
 
@@ -71,7 +89,7 @@ INSERT INTO `event` (`id`, `name`, `count`, `category_id`) VALUES
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `name` varchar(70) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
@@ -80,7 +98,10 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `name`) VALUES
 (1, 'Дьячок Михаил Сергеевич'),
 (2, 'Бикулин Павел Петрович'),
-(3, 'Абушинов Александр Александрович');
+(3, 'Абушинов Александр Александрович'),
+(4, 'Гусаченко Роман Юрьевич'),
+(5, 'Хрипченко Сергей Анатольевич'),
+(8, 'Потапов Александр Алексеевич');
 
 -- --------------------------------------------------------
 
@@ -92,6 +113,8 @@ CREATE TABLE IF NOT EXISTS `users_event` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `event_id` int(11) NOT NULL,
+  `comment` text,
+  `room_id` int(11) DEFAULT NULL,
   `count` int(11) NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
@@ -100,12 +123,11 @@ CREATE TABLE IF NOT EXISTS `users_event` (
 -- Дамп данных таблицы `users_event`
 --
 
-INSERT INTO `users_event` (`id`, `user_id`, `event_id`, `count`, `date`) VALUES
-(1, 3, 1, 1, '2017-08-26'),
-(2, 2, 2, 12, '2017-05-11'),
-(3, 1, 3, 27, '2017-06-02'),
-(4, 2, 4, 128, '2017-07-14'),
-(5, 3, 4, 20, '2017-08-28');
+INSERT INTO `users_event` (`id`, `user_id`, `event_id`, `comment`, `room_id`, `count`, `date`) VALUES
+(2, 2, 5, NULL, 1, 1, '2017-08-30'),
+(3, 2, 6, 'Строевой исправление кривизны рук и восприятия мира', 1, 7, '2017-08-11'),
+(4, 2, 7, 'из кабинета 204  4-й учебный корпус в кабинет 306, 4-1 учебный корпус', NULL, 1, '2017-08-22'),
+(5, 2, 8, 'Ляпота, 2 дня', 1, 2, '2017-08-26');
 
 --
 -- Индексы сохранённых таблиц
@@ -121,6 +143,12 @@ ALTER TABLE `categories`
 -- Индексы таблицы `event`
 --
 ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `room`
+--
+ALTER TABLE `room`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -148,12 +176,17 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT для таблицы `event`
 --
 ALTER TABLE `event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT для таблицы `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT для таблицы `users_event`
 --
